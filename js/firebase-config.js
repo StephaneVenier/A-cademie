93e0bfcdf83d9f9621a81e6c7292a441c4c749af
StephaneVenier <stephane.venier85@gmail.com>
2025-11-19 09:10:40 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,13 +4,15 @@
 
 if (typeof firebase !== "undefined") {
   const firebaseConfig = {
-    apiKey: "REMPLACE-MOI",
-    authDomain: "REMPLACE-MOI.firebaseapp.com",
-    projectId: "REMPLACE-MOI",
-    storageBucket: "REMPLACE-MOI.appspot.com",
-    messagingSenderId: "REMPLACE-MOI",
-    appId: "REMPLACE-MOI"
-  };
+  apiKey: "AIzaSyC5zlM-40tu4k3xD3rJH13HR-mh8D9_JM0",
+  authDomain: "sporting-anywhere.firebaseapp.com",
+  databaseURL: "https://sporting-anywhere-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "sporting-anywhere",
+  storageBucket: "sporting-anywhere.firebasestorage.app",
+  messagingSenderId: "1053011055365",
+  appId: "1:1053011055365:web:6fc7d8e048520d0eca1d6c",
+  measurementId: "G-5YZ2H39PVZ"
+};
 
   if (firebase.apps && !firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
